91b1c8df6ef6ef709d7bf66f0c965989f7cd64a1
rolandosorbelli <rolando.sorbelli@gmail.com>
2017-11-06 11:20:13 +0000

    Created a list of items

--- src/App.js
+++ src/App.js
@@ -4,12 +4,20 @@ import Sales from './Sales';
 
 class App extends Component {
   render() {
+
+    var books = [
+      { title: "A little life", author: "Hanya Yanagihara", price: 8.99},
+      { title: "The power", author: "Naomi Alderman", price: 9.99},
+      { title: "Homo Deus", author: "Yuval Noah Harari", price: 12.99},
+      { title: "Norse Mythology", author: "Neil Gaiman", price: 18.99},
+    ];
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome to Sales App</h1>
         </header>
-        <Sales />
+        <Sales items={books} />
       </div>
     );
   }
--- src/Sales.js
+++ src/Sales.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 class Sales extends Component {
   render() {
+    	console.log(this.props.items)
     return (
       <div>
         This is Sales Component
