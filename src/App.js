de933843cc63deca72d52261ec69af2925df55a8
rolandosorbelli <rolando.sorbelli@gmail.com>
2017-11-06 11:07:17 +0000

    Created Sales component into a separate file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Sales from './Sales';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Sales App</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Sales />
       </div>
     );
   }
---
+++ src/Sales.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Sales extends Component {
+  render() {
+    return (
+      <div>
+        This is Sales Component
+      </div>
+    );
+  }
+}
+
+export default Sales;
