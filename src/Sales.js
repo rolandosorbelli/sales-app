17fb4fa124a3257069640879669ce09a4c01ccad
rolandosorbelli <rolando.sorbelli@gmail.com>
2017-11-06 11:47:05 +0000

    Created a new component
    - Book component has been created
    - Iterated through the books props to display every element

--- src/Sales.js
+++ src/Sales.js
@@ -12,8 +12,26 @@ class Sales extends Component {
 		this.state = {
 			total: 0
 		}
+		this.sumPrice = this.sumPrice.bind(this)
 	}
 
+  render() {
+  	var books = this.props.items.map((item, i) => {
+  		return <Books title={item.title} author={item.author}
+  		price={item.price} key={i} sumPrice={this.sumPrice}
+  		active={item.active}
+  		 />
+  	});
+
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+class Books extends Component {
   render() {
     return (
       <div>
